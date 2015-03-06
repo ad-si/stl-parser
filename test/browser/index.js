da80ab79ceac92f69e38f71e8d61502ac225751f
Adrian Sieber <mail@adriansieber.com>
2015-03-06 21:33:43 +0100

    Add streamed stl-loading to test-website

--- test/browser/index.js
+++ test/browser/index.js
@@ -1,27 +1,77 @@
-var stlImporter = require('../../src/index'),
-	reader = new FileReader()
+var ReadableFileStream = require('filestream').read,
+
+	//ReadableFileStream = require('filereader-stream'),
+
+	stlImporter = require('../../src/index'),
+
+	bufferedContainer = document.getElementById('bufferedLoading'),
+	streamedContainer = document.getElementById('streamedLoading')
 
 
-function handleFileSelect (events) {
+function loadBuffered (changeEvent) {
 
-	var files = events.target.files,
-		filesArray = Array(files)
+	var files = changeEvent.target.files,
+		filesArray = new Array(files),
+		reader = new FileReader()
+
+	reader.addEventListener('load', function (file) {
+
+		stlImporter(file.target.result)
+			.on('data', function (data) {
+				console.log('Loaded', data)
+				bufferedContainer
+					.querySelector('p')
+					.textContent = 'Loaded "' + data.name + '"'
+			})
+	})
 
 	filesArray.forEach(function (file, index) {
 		reader.readAsArrayBuffer(files[index])
 	})
 }
 
-reader.addEventListener('load', function (file) {
+function loadStreamed (changeEvent) {
 
-	console.log(file)
-	stlImporter(file.target.result)
-		.on('data', function (data) {
-			console.log('test', data)
-		})
-})
+	var files = changeEvent.target.files,
+		progress = document.querySelector('progress'),
+		stlParser = stlImporter(),
+		fileStream = new ReadableFileStream(files[0]),
+		counter = 0
 
-document
-	.getElementById('filesInput')
-	.addEventListener('change', handleFileSelect, false)
 
+	stlParser.on('data', function (data) {
+
+		counter++
+
+		if (counter % 1000 === 1)
+			streamedContainer
+				.querySelector('p')
+				.textContent = 'Face "' + data.number + '"'
+	})
+
+	fileStream.reader.addEventListener('progress', function (event) {
+
+		var percentageLoaded = 0
+
+		if (event.lengthComputable) {
+			percentageLoaded = (event.loaded / event.total).toFixed(2)
+
+			progress.setAttribute('value', percentageLoaded)
+		}
+	})
+
+	fileStream.on('error', function (error) {
+		throw error
+	})
+
+	fileStream.pipe(stlParser)
+}
+
+
+bufferedContainer
+	.querySelector('input')
+	.addEventListener('change', loadBuffered)
+
+streamedContainer
+	.querySelector('input')
+	.addEventListener('change', loadStreamed)
